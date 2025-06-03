20888b68797dc529405ef3c031aaf1defad287a7
Nadeem <nadeem@fileverse.io>
2025-06-03 15:32:00 +0530

    allow all

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,6 @@ app.use(logger("combined"));
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(
