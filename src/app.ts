62b2366b5769ba93f38fd25f697d21ef2540c4b6
Nadeem <nadeem@fileverse.io>
2025-06-03 15:22:08 +0530

    update cors

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,13 @@ app.use(express.json());
 
 // Use default logger for now
 app.use(logger("combined"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(
   helmet({
     contentSecurityPolicy: false,
