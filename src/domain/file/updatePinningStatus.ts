21665b4e4f445788a0f82120c5a1a94f2de7a89a
Nadeem <nadeem@fileverse.io>
2025-07-15 11:18:33 +0530

    minor optimisations

--- src/domain/file/updatePinningStatus.ts
+++ src/domain/file/updatePinningStatus.ts
@@ -1,8 +1,14 @@
 import { File } from "../../infra/database/models";
 
 export const updatePinningStatus = async (
-  mongoObjectId: string,
+  mongoObjectId: string | string[],
   isPinned: boolean
 ) => {
+  if (Array.isArray(mongoObjectId)) {
+    return await File.updateMany(
+      { _id: { $in: mongoObjectId } },
+      { $set: { isPinned } }
+    );
+  }
   return await File.updateOne({ _id: mongoObjectId }, { $set: { isPinned } });
 };
--- src/interface/agenda/jobs/unpinDeletedFileHashes.ts
+++ src/interface/agenda/jobs/unpinDeletedFileHashes.ts
@@ -24,34 +24,45 @@ async function jobDefinition(job: Job, done: (args?: any) => void) {
 
 async function setupJob() {
   agenda.define(JOB_NAME, jobDefinition);
-  agenda.every("1 minute", JOB_NAME);
+  agenda.every("5 minute", JOB_NAME);
 }
+
+const waitFor = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
 async function unpinDeletedFileHashes() {
   const files = await File.find({
     isPinned: true,
     isDeleted: true,
     markedForUnpin: true,
-  }).limit(MAX_BATCH_SIZE);
+  })
+    .sort({ createdAt: -1 })
+    .limit(MAX_BATCH_SIZE);
 
   logger.info(`Unpinning ${files.length} deleted file hashes`);
+  const unpinnedFileIds = [];
 
   for (const file of files) {
     const mongoObjectId = file._id.toString();
     try {
       if (file.ipfsHash) {
         await unpin(file.ipfsHash);
-        await updatePinningStatus(mongoObjectId, false);
+        unpinnedFileIds.push(mongoObjectId);
+        await waitFor(300);
         logger.info(`Unpinned ${file.ipfsHash}`);
       }
     } catch (error: any) {
       if (error && error.reason === "CURRENT_USER_HAS_NOT_PINNED_CID") {
-        await updatePinningStatus(mongoObjectId, false);
+        unpinnedFileIds.push(mongoObjectId);
         logger.info(
           `Updated DB State only for ${file.ipfsHash}, because already unpinned`
         );
       }
     }
+  }
+
+  if (unpinnedFileIds.length > 0) {
+    await updatePinningStatus(unpinnedFileIds, false);
   }
 }
 
