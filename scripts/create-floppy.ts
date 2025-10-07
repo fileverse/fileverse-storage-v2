cdc71babb6405a6fc47c8cd1f539c758e5eea86a
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2025-10-07 10:33:45 +0530

    removed unncessary data

--- scripts/create-floppy.ts
+++ scripts/create-floppy.ts
@@ -1,34 +1,7 @@
 // create a floppy with the following details
 import Floppy from "../src/infra/database/models/floppy.ts";
 
-const data = [
-  {
-    shortCode: "FLV",
-    name: "Floppy #001",
-    description: "Kawai floppy by fileverse!",
-    img: "https://ddocs-new-git-maitra-tec-1139-fileverse.vercel.app/assets/floppy1.png",
-    metadataURI:
-      "ipfs://bafkreic3h7bfs4ifq7kz6wot4b7qyqo3uq3plp4pbf37flg2up5joxkfwq",
-    diskSpace: 1000000000,
-    offchain: true,
-    networkName: "offchain",
-    members: [],
-    sgid: "n/a",
-  },
-  {
-    shortCode: "OXB",
-    name: "OXB #001",
-    description: "Floppy for oxford freshers fair participants by fileverse!",
-    img: "https://ddocs-new-git-maitra-tec-1139-fileverse.vercel.app/assets/floppy2.png",
-    metadataURI:
-      "ipfs://bafkreic3h7bfs4ifq7kz6wot4b7qyqo3uq3plp4pbf37flg2up5joxkfwq",
-    diskSpace: 1000000000,
-    offchain: true,
-    networkName: "offchain",
-    members: [],
-    sgid: "n/a",
-  },
-];
+const data = [];
 
 const createFloppy = async () => {
   for (const item of data) {
