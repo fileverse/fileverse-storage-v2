a95375e595461ce3240665c95f624c412a610e7c
Swagnik Dutta <swagnikd@gmail.com>
2026-03-24 18:53:51 +0530

    minor: fix typescript warning

--- src/interface/middleware/webhookAuth.ts
+++ src/interface/middleware/webhookAuth.ts
@@ -3,7 +3,8 @@ import { Request, Response, NextFunction } from "express";
 export const webhookAuth = (req: Request, res: Response, next: NextFunction) => {
   const apiKey = req.headers["x-webhook-api-key"];
   if (!apiKey || apiKey !== process.env.INDEXER_WEBHOOK_API_KEY) {
-    return res.status(401).json({ error: "Unauthorized" });
+    res.status(401).json({ error: "Unauthorized" });
+    return;
   }
   next();
 };
